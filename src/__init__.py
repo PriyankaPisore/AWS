99688bfdbc4c482cc91697e5af0b299412290b10
pranit-p <pranit.pawar@selfscore.com>
2024-02-19 23:33:20 +0530

    Add full functionality of chatbot

--- src/__init__.py
+++ src/__init__.py
@@ -10,20 +10,8 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
-    # pyngrok.init_app(app)
-
-    # if test_config is None:
-    #     app.config.from_mapping(
-    #         SECRET_KEY=os.environ.get("SECRET_KEY"),
-    #         SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URL"),
-    #         SQLALCHEMY_TRACK_MODIFICATIONS=False
-    #     )
-
-    # db.app = app
-    # db.init_app(app)
-    print("hello")
+   
+    
     app.register_blueprint(chat)
-    print("hello1")
-    # app.register_blueprint(sentence)
-    # db.create_all()
+    
     return app
--- src/chat.py
+++ src/chat.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, request, jsonify, render_template
+import pandas as pd
+import numpy as np
+import torch
+from transformers import pipeline
+from transformers import TapasConfig, TapasForQuestionAnswering
+
 
 chat = Blueprint("chat", __name__, url_prefix="/chat")
+tqa = pipeline(task="table-question-answering",model="google/tapas-large-finetuned-wtq")
+dataset = pd.read_csv("./data_sheet.csv")
+dataset = dataset.astype(str)
 
 @chat.route("/", methods=["GET", "POST"])
 def display_sentence():
+    
     return render_template("./index.html")
 
 
@@ -12,5 +22,5 @@ def ask():
     # chat_question = request.form['chat_box_input']
     request_parameter = request.get_json(force=True)
     chat_question = request_parameter["chat_box_input"]
-    return (chat_question)
+    return (tqa(table=dataset,query=chat_question))
 
