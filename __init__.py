b99e173a5703e80484b8bafe773b0493072e117a
pranit-p <pranit.pawar@selfscore.com>
2024-02-23 16:29:03 +0530

    Update project from folder structure to files

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
 import os
-from chat import chat
 from flask_cors import CORS, cross_origin
 from pyngrok import ngrok
 import threading
+from flask import Blueprint, request, jsonify, render_template
+import pandas as pd
+import numpy as np
+import torch
+from transformers import pipeline
+from transformers import TapasConfig, TapasForQuestionAnswering, TapasTokenizer
+from transformers import AutoTokenizer, AutoModelForTableQuestionAnswering
+from transformers import TapexTokenizer, BartForConditionalGeneration, pipeline
 
 # from flask_caching import Cache
 os.environ['FLASK_ENV'] = "development"
@@ -24,6 +31,37 @@ print(f"url is {public_url} and port is {port}")
 
 # cache = Cache(app)
 # cache.clear()
-app.register_blueprint(chat)
 threading.Thread(target=app.run,kwargs={"use_reloader": False}).start()
 # return app
+
+tqa = pipeline(task="table-question-answering",model="google/tapas-large-finetuned-wtq")
+dataset = pd.read_csv("./data_sheet.csv")
+dataset = dataset.reset_index(drop=True)
+# dataset['Number of Objects'] = dataset['Number of Objects'].astype(str)
+# dataset['Bill Per Month'] = dataset['Bill Per Month'].astype(str)
+dataset = dataset.astype(str)
+
+
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def display_sentence():
+    return render_template("./index.html")
+
+
+@app.route("/chat/ask", methods=["POST"])
+def ask():
+    # chat_question = request.form['chat_box_input']
+    request_parameter = request.get_json(force=True)
+    chat_question = request_parameter["chat_box_input"]
+    # response = dl.chat(chat_question)
+    # return response
+
+
+    # encoding = tokenizer(table=dataset, query=chat_question,return_tensors="pt")
+    # outputs = model.generate(**encoding)
+    # result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    # return(result)
+    # return(apply_model(dataset, [chat_question]))
+
+    return (tqa(table=dataset,query=chat_question))
